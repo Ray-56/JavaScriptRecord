5eb6f917e1429967f5031669578e70a1790b72ea
782216385@qq.com <782216385@qq.com>
2019-04-17 10:26:00 +0800

    no message

--- algorithms/demo.js
+++ algorithms/demo.js
@@ -1,4 +1,79 @@
 global.log = console.log.bind(console);
 const arr = [10, 3, 6, 4, 2, 1, 7, 9, 8, 5];
 
-// 实现 new 操作符
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                const temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    console.log(arr);
+}
+
+// 改进冒泡排序1
+function bubbleSort1(arr) {
+    let i = arr.length - 1;
+
+    while (i > 0) {
+        let pos = 0;
+        for (let j = 0; j < i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                pos = j;
+                const temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        i = pos;
+    }
+    console.log(arr);
+}
+
+
+// 改进冒泡排序2
+function bubbleSort2(arr) {
+    let low = 0;
+    let high = arr.length - 1;
+    let temp, j;
+
+    while (low < high) {
+        // 正排找最大
+        for (j = low; j < high; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        --high;
+
+        // 反排找最小
+        for (j = high; j > low; --j) {
+            if (arr[j] < arr[j - 1]) {
+                temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+            }
+        }
+        ++low;
+    }
+    console.log(arr);
+}
+
+function parent(age) {
+    this.age = age;
+}
+
+function parent1(age) {
+    return {
+        age
+    }
+}
+
+const p1 = parent(19);
+const p2 = Number(44);
+console.log(Object.prototype.toString.call(p2));
---
+++ interview/questions.md
@@ -0,0 +1,68 @@
+# 题（topic）
+
+总结一些面试题
+
+## http
+
+1.  http1.0、http1.1、http2.0 以及 https
+1.  http 缓存
+1.  常见错误码，304代表什么
+1.  常见请求头、响应头
+1.  http 响应头中 Keep-Alive
+1.  tcp 与 http 的关系
+
+## vue
+
+1.  vue 生命周期
+1.  vue 实现原理
+1.  vue 计算属性（computed）如何实现
+1.  vue Object.defineProperty 缺点，以及在 3.0 使用什么代替，优点有什么
+1.  vue 如何做到监听数组。接上面的缺点
+1.  vuex 思想、以及使用
+
+## webpack
+
+1.  webpack HRM（热更新）
+1.  webpack 原理
+1.  webpack 打包优化
+
+## 浏览器
+
+1.  浏览器渲染原理
+1.  重绘、回流
+
+## 优化
+
+1.  cdn 原理
+1.  资源为什么要改为很多不同的域名加载
+
+## js
+
+1.  js eventloop
+1.  js 值与址 衍生 深浅拷贝
+1.  js 闭包
+1.  js new 操作符做了什么
+1.  js 原型以及原型链
+1.  原型链最终指向
+1.  async 如何实现
+1.  类数组如何转化为数组有哪些方式
+1.  es6 解构
+1.  es6 promise 实现
+1.  promise.all() 与 promise.race()
+1.  promise 如何实现 .finally()
+
+## node
+
+1.  node eventloop，与浏览器的有何不同
+
+## css
+
+1.  css 水平垂直居中
+
+## 其它
+
+1.  TypeScript 相关
+1.  docker
+1.  nginx 优化
+1.  设计模式都了解哪些
+1.  git 操作
